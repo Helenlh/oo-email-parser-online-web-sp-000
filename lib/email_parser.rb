b519d27b2b9104343be943904492268967ccf0c1
HELEN HARMAN <hlharman@bellsouth.net>
2021-01-29 00:21:40 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -9,12 +9,13 @@ class EmailAddressParser
   
   def parse
     #if emailaddress.include? ","
-     new_address = emailaddress.split.(/, | /)
+     new_address = emailaddress.split(/, | /)
      #if new_address.include? ","
   #   csv_address.uniq
   # else
   #   non_csv_address = emailaddress.split(" ",)
      #non_csv_address.uniq 
+     new_address.uniq 
      end
    end 
 end 
